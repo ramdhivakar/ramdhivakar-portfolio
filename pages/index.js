e94f742e4edf2d3b1a90aa34dc89814a4808ee8c
ramdhivakar <ramdhivakar@yahoo.com>
2024-01-18 07:39:00 +0530

    commit before with avatar

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ const Home = () => {
             animate="show" 
             exit="hidden" 
             transition={{duration: 1, ease: 'easeInOut'}}
-            className="w-full h-full max-w-[637px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+            className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
           <Avatar />
         </motion.div>
       </div>
