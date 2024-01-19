48bfe33f0a21c497a107d6ff7e36b9e36dc6aaff
ramdhivakar <ramdhivakar@yahoo.com>
2024-01-19 15:15:36 +0530

    profile links

--- components/Socials.js
+++ components/Socials.js
@@ -4,10 +4,10 @@ import { RiGithubFill, RiLinkedinBoxFill} from 'react-icons/ri';
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      <Link href={''} className="hover:text-accent transition-all duration-300 ">
+      <Link href={'https://github.com/ramdhivakar'} className="hover:text-accent transition-all duration-300 ">
         <RiGithubFill />
       </Link>
-      <Link href={''} className="hover:text-accent transition-all duration-300 ">
+      <Link href={'www.linkedin.com/in/ram--dhivakar'} className="hover:text-accent transition-all duration-300 ">
         <RiLinkedinBoxFill />
       </Link>
     </div>
